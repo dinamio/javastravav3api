e208c314f81dff0baa7672e34254d3bc5ca83c41
Eugen Godun <ievgen.godun@bolt.eu>
2025-09-04 18:17:55 +0300

    Reflect new API changes

--- Strava API v3/src/javastrava/api/StreamAPI.java
+++ Strava API v3/src/javastrava/api/StreamAPI.java
@@ -2,8 +2,7 @@ package javastrava.api;
 
 import javastrava.api.async.StravaAPICallback;
 import javastrava.model.StravaStream;
-import javastrava.model.reference.StravaStreamResolutionType;
-import javastrava.model.reference.StravaStreamSeriesDownsamplingType;
+import javastrava.model.reference.StravaStreamType;
 import javastrava.service.StreamService;
 import javastrava.service.exception.BadRequestException;
 import javastrava.service.exception.NotFoundException;
@@ -23,16 +22,32 @@ import retrofit.http.Query;
  */
 public interface StreamAPI {
 	/**
-	 * @see javastrava.service.StreamService#getActivityStreams(Long, StravaStreamResolutionType, StravaStreamSeriesDownsamplingType, javastrava.model.reference.StravaStreamType...)
+	 * Converts an array of StravaStreamType enums to a comma-separated string for API calls
+	 * @param types Array of stream types
+	 * @return Comma-separated string of stream type values
+	 */
+	static String typeString(final StravaStreamType[] types) {
+		if (types == null || types.length == 0) {
+			return null;
+		}
+		if (types.length == 1) {
+			return types[0].getValue();
+		}
+		StringBuilder typeString = new StringBuilder(types[0].getValue());
+		for (int i = 1; i < types.length; i++) {
+			typeString.append(",").append(types[i].getValue());
+		}
+		return typeString.toString();
+	}
+	/**
+	 * @see javastrava.service.StreamService#getActivityStreams(Long, javastrava.model.reference.StravaStreamType...)
 	 *
 	 * @param activityId
 	 *            The id of the activity for which streams are to be retrieved
 	 * @param types
-	 *            List of types, if the activity does not have that stream it will not be included in the response
-	 * @param resolution
-	 *            (Optional) low (100), medium (1000) or high (10000), default is all, indicates desired number of data points, streams will only be down sampled
-	 * @param seriesType
-	 *            (Optional) relevant only if using resolution. Either "time" or "distance", default is "distance", used to index the streams if the stream is being reduced
+	 *            Array of stream types, if the activity does not have that stream it will not be included in the response
+	 * @param keyByType
+	 *            (Optional) Used to alter the output of the returned streams. If true, the keys of streams will be their types
 	 * @return Returns an array of unordered stream objects, or <code>null</code> if the activity doesn't exist
 	 * @throws UnauthorizedException
 	 *             If there is a security exception
@@ -41,21 +56,40 @@ public interface StreamAPI {
 	 * @throws BadRequestException
 	 *             If the request is malformed
 	 */
-	@GET("/activities/{id}/streams/{types}")
-	public StravaStream[] getActivityStreams(@Path("id") final Long activityId, @Path("types") final String types, @Query("resolution") final StravaStreamResolutionType resolution,
-			@Query("series_type") final StravaStreamSeriesDownsamplingType seriesType) throws UnauthorizedException, NotFoundException, BadRequestException;
+	@GET("/activities/{id}/streams/")
+	default StravaStream[] getActivityStreams(@Path("id") final Long activityId, final StravaStreamType[] types, @Query("key_by_type") final Boolean keyByType) throws UnauthorizedException, NotFoundException, BadRequestException {
+		return getActivityStreams(activityId, typeString(types), keyByType);
+	}
 
 	/**
-	 * @see javastrava.service.StreamService#getActivityStreams(Long, StravaStreamResolutionType, StravaStreamSeriesDownsamplingType, javastrava.model.reference.StravaStreamType...)
+	 * @see javastrava.service.StreamService#getActivityStreams(Long, javastrava.model.reference.StravaStreamType...)
+	 *
+	 * @param activityId
+	 *            The id of the activity for which streams are to be retrieved
+	 * @param keys
+	 *            Comma separated list of types, if the activity does not have that stream it will not be included in the response
+	 * @param keyByType
+	 *            (Optional) Used to alter the output of the returned streams. If true, the keys of streams will be their types
+	 * @return Returns an array of unordered stream objects, or <code>null</code> if the activity doesn't exist
+	 * @throws UnauthorizedException
+	 *             If there is a security exception
+	 * @throws NotFoundException
+	 *             If the activity does not exist
+	 * @throws BadRequestException
+	 *             If the request is malformed
+	 */
+	@GET("/activities/{id}/streams/")
+	public StravaStream[] getActivityStreams(@Path("id") final Long activityId, @Query("keys") final String keys, @Query("key_by_type") final Boolean keyByType) throws UnauthorizedException, NotFoundException, BadRequestException;
+
+	/**
+	 * @see javastrava.service.StreamService#getActivityStreams(Long, javastrava.model.reference.StravaStreamType...)
 	 *
 	 * @param activityId
 	 *            The id of the activity for which streams are to be retrieved
 	 * @param types
-	 *            List of types, if the activity does not have that stream it will not be included in the response
-	 * @param resolution
-	 *            (Optional) low (100), medium (1000) or high (10000), default is all, indicates desired number of data points, streams will only be down sampled
-	 * @param seriesType
-	 *            (Optional) relevant only if using resolution. Either "time" or "distance", default is "distance", used to index the streams if the stream is being reduced
+	 *            Array of stream types, if the activity does not have that stream it will not be included in the response
+	 * @param keyByType
+	 *            (Optional) Used to alter the output of the returned streams. If true, the keys of streams will be their types
 	 * @param callback
 	 *            The callback to execute on completion
 	 * @throws UnauthorizedException
@@ -65,22 +99,43 @@ public interface StreamAPI {
 	 * @throws BadRequestException
 	 *             If the request is malformed
 	 */
-	@GET("/activities/{id}/streams/{types}")
-	public void getActivityStreams(@Path("id") final Long activityId, @Path("types") final String types, @Query("resolution") final StravaStreamResolutionType resolution,
-			@Query("series_type") final StravaStreamSeriesDownsamplingType seriesType, final StravaAPICallback<StravaStream[]> callback)
+	@GET("/activities/{id}/streams/")
+	default void getActivityStreams(@Path("id") final Long activityId, final StravaStreamType[] types, @Query("key_by_type") final Boolean keyByType, final StravaAPICallback<StravaStream[]> callback)
+			throws UnauthorizedException, NotFoundException, BadRequestException {
+		getActivityStreams(activityId, typeString(types), keyByType, callback);
+	}
+
+	/**
+	 * @see javastrava.service.StreamService#getActivityStreams(Long, javastrava.model.reference.StravaStreamType...)
+	 *
+	 * @param activityId
+	 *            The id of the activity for which streams are to be retrieved
+	 * @param keys
+	 *            Comma separated list of types, if the activity does not have that stream it will not be included in the response
+	 * @param keyByType
+	 *            (Optional) Used to alter the output of the returned streams. If true, the keys of streams will be their types
+	 * @param callback
+	 *            The callback to execute on completion
+	 * @throws UnauthorizedException
+	 *             If there is a security exception
+	 * @throws NotFoundException
+	 *             If the activity does not exist
+	 * @throws BadRequestException
+	 *             If the request is malformed
+	 */
+	@GET("/activities/{id}/streams/")
+	public void getActivityStreams(@Path("id") final Long activityId, @Query("keys") final String keys, @Query("key_by_type") final Boolean keyByType, final StravaAPICallback<StravaStream[]> callback)
 			throws UnauthorizedException, NotFoundException, BadRequestException;
 
 	/**
-	 * @see javastrava.service.StreamService#getActivityStreams(Long, StravaStreamResolutionType, StravaStreamSeriesDownsamplingType, javastrava.model.reference.StravaStreamType...)
+	 * @see javastrava.service.StreamService#getActivityStreams(Long, javastrava.model.reference.StravaStreamType...)
 	 *
 	 * @param activityId
 	 *            The id of the activity for which streams are to be retrieved
 	 * @param types
-	 *            List of types, if the activity does not have that stream it will not be included in the response
-	 * @param resolution
-	 *            (Optional) low (100), medium (1000) or high (10000), default is all, indicates desired number of data points, streams will only be down sampled
-	 * @param seriesType
-	 *            (Optional) relevant only if using resolution. Either "time" or "distance", default is "distance", used to index the streams if the stream is being reduced
+	 *            Array of stream types, if the activity does not have that stream it will not be included in the response
+	 * @param keyByType
+	 *            (Optional) Used to alter the output of the returned streams. If true, the keys of streams will be their types
 	 * @return Returns an array of unordered stream objects, or <code>null</code> if the activity doesn't exist
 	 * @throws UnauthorizedException
 	 *             If there is a security exception
@@ -89,9 +144,30 @@ public interface StreamAPI {
 	 * @throws BadRequestException
 	 *             If the request is malformed
 	 */
-	@GET("/activities/{id}/streams/{types}")
-	public Response getActivityStreamsRaw(@Path("id") final Long activityId, @Path("types") final String types, @Query("resolution") final StravaStreamResolutionType resolution,
-			@Query("series_type") final StravaStreamSeriesDownsamplingType seriesType) throws UnauthorizedException, NotFoundException, BadRequestException;
+	@GET("/activities/{id}/streams/")
+	default Response getActivityStreamsRaw(@Path("id") final Long activityId, final StravaStreamType[] types, @Query("key_by_type") final Boolean keyByType) throws UnauthorizedException, NotFoundException, BadRequestException {
+		return getActivityStreamsRaw(activityId, typeString(types), keyByType);
+	}
+
+	/**
+	 * @see javastrava.service.StreamService#getActivityStreams(Long, javastrava.model.reference.StravaStreamType...)
+	 *
+	 * @param activityId
+	 *            The id of the activity for which streams are to be retrieved
+	 * @param keys
+	 *            Comma separated list of types, if the activity does not have that stream it will not be included in the response
+	 * @param keyByType
+	 *            (Optional) Used to alter the output of the returned streams. If true, the keys of streams will be their types
+	 * @return Returns an array of unordered stream objects, or <code>null</code> if the activity doesn't exist
+	 * @throws UnauthorizedException
+	 *             If there is a security exception
+	 * @throws NotFoundException
+	 *             If the activity does not exist
+	 * @throws BadRequestException
+	 *             If the request is malformed
+	 */
+	@GET("/activities/{id}/streams/")
+	public Response getActivityStreamsRaw(@Path("id") final Long activityId, @Query("keys") final String keys, @Query("key_by_type") final Boolean keyByType) throws UnauthorizedException, NotFoundException, BadRequestException;
 
 	/**
 	 * @see javastrava.service.StreamService#getEffortStreams(Long, StravaStreamResolutionType, StravaStreamSeriesDownsamplingType, javastrava.model.reference.StravaStreamType...)
